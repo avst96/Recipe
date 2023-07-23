00fe45d98362c59b1b39ba70b7f0934a74c4cf98
avst96 <avst96@gmail.com>
2023-07-23 14:59:46 -0500

    Removed extra code from middle tier

--- RecipeApps/RecipeAppSystem/RecipeSystem.cs
+++ RecipeApps/RecipeAppSystem/RecipeSystem.cs
@@ -11,16 +11,7 @@ namespace RecipeAppSystem
             string sql = "select RecipeID, RecipeName, Calories, DateDrafted, DatePublished, DateArchived, RecipeStatus, RecipePic from Recipe where RecipeName like '%" + recipename + "%'";
             return SQLUtility.GetDataTable(sql);
         }
-        
-        public static int SelectRecipe(DataTable dt, int rowindex)
-        {
-            int recipeid = 0;
-            if (rowindex >= 0)
-            {
-                recipeid = (int)dt.Rows[rowindex]["RecipeID"];
-            }
-            return recipeid;
-        }
-
+        //TODO Continue to add middle tier code here
+       
     }
 }
