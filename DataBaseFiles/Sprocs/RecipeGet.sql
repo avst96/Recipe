02bd7da25859033646bd2b2bcc95de20642c9a31
avst96 <avst96@gmail.com>
2023-08-20 09:27:08 -0500

    recipe table sprocs done

--- DataBaseFiles/Sprocs/RecipeGet.sql
+++ DataBaseFiles/Sprocs/RecipeGet.sql
@@ -1,12 +1,32 @@
 use HeartyHearthDB
 go
 
-create or alter procedure dbo.RecipeGet 
+create or alter procedure dbo.RecipeGet (@RecipeId int = 0, @RecipeName varchar(50) = '', @All bit = 0)
 as 
 begin
-select r.RecipeID, r.UsersID, r.CuisineID, r.RecipeName, r.Calories, r.DateDrafted, r.DatePublished, r.DateArchived, r.RecipeStatus, r.RecipePic
-from Recipe r
+	select @RecipeName = nullif (@RecipeName, '')
+	
+	select r.RecipeID, r.UsersID, r.CuisineID, r.RecipeName, r.Calories, r.DateDrafted, r.DatePublished, r.DateArchived, r.RecipeStatus, r.RecipePic
+	from Recipe r
+	where r.RecipeID = @RecipeId
+	or r.RecipeName like '%' + @RecipeName + '%'
+	or @All = 1
 end
 go
 
-exec RecipeGet
+/*
+exec RecipeGet --Returns empty result set
+
+exec RecipeGet @All = 1
+
+declare @ID int 
+select top 1 @ID = RecipeID from Recipe order by RecipeID
+exec RecipeGet @RecipeId = @ID
+
+exec RecipeGet @RecipeName = ''
+
+exec RecipeGet @RecipeName = 'mu'
+
+exec RecipeGet @RecipeName = null
+*/
+
