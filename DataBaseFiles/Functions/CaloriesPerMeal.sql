4fc00579bfdd3297d3004a47078804b8d6ef47af
avst96 <avst96@gmail.com>
2023-09-11 17:58:32 -0500

    removed unneeded table from function

--- DataBaseFiles/Functions/CaloriesPerMeal.sql
+++ DataBaseFiles/Functions/CaloriesPerMeal.sql
@@ -6,14 +6,12 @@ returns int
 as begin
 	declare @calories int
 	select @calories = sum(r.Calories)
-	from Meal m
-	join MealCourse mc 
-	on m.MealID = mc.MealID
+	from MealCourse mc 
 	join MealCourseRecipe mcr 
 	on mc.MealCourseID = mcr.MealCourseID
 	join Recipe r
 	on mcr.RecipeID = r.RecipeID
-	where m.MealID = @MealId
+	where mc.MealID = @MealId
 
 	return @calories
 end
