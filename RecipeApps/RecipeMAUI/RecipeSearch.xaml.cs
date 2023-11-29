eb553af04245e87b9bb3ac92f73614271aea1a3e
avst96 <avst96@gmail.com>
2023-11-29 17:39:11 -0600

    HW 32 done

--- RecipeApps/RecipeMAUI/RecipeSearch.xaml.cs
+++ RecipeApps/RecipeMAUI/RecipeSearch.xaml.cs
@@ -1,9 +1,19 @@
+using RecipeAppSystem;
+using System.Data;
+
 namespace RecipeMAUI;
 
 public partial class RecipeSearch : ContentPage
 {
-	public RecipeSearch()
-	{
-		InitializeComponent();
-	}
+    public RecipeSearch()
+    {
+        InitializeComponent();
+        this.Loaded += RecipeSearch_Loaded;
+    }
+
+    private void RecipeSearch_Loaded(object sender, EventArgs e)
+    {
+        DataTable dt = RecipeSystem.GetRecipeSummary();
+        RecipeLst.ItemsSource = dt.Rows;
+    }
 }
